3a15137bd4618913c1fdf20239d669780097d48c
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-09-25 22:35:10 -0300

    Completando o desafio conforme expecificações do professor

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,17 +15,12 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
+                throw new Exception("A capacidade disponível é menor do que o número de hóspedes.");
         }
 
         public void CadastrarSuite(Suite suite)
@@ -35,25 +30,17 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal valorDesconto = valor * Convert.ToDecimal(0.10);
+                valor -= valorDesconto;
             }
-
             return valor;
         }
     }
--- Program.cs
+++ Program.cs
@@ -2,24 +2,27 @@
 using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
+            List<Pessoa> hospedes = new List<Pessoa>();
+            Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+            Pessoa p2 = new Pessoa(nome: "Hóspede 2");
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
+            hospedes.Add(p1);
+            hospedes.Add(p2);
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+            // Cria a suíte
+            Console.Write("Qual será o tipo da Suíte? ");
+            string tipoSuite = Console.ReadLine();
+            Console.Write("Qual a capacidade da Suíte? ");
+            int capacidade = int.Parse(Console.ReadLine());
+            Console.Write("Qual a diária da Suíte escolhida? ");
+            decimal valorDiaria = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Suite suite = new Suite(tipoSuite: tipoSuite, capacidade: capacidade, valorDiaria: valorDiaria);
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+            // Cria uma nova reserva, passando a suíte e os hóspedes
+            Reserva reserva = new Reserva(diasReservados: 10);
+            reserva.CadastrarSuite(suite);
+            reserva.CadastrarHospedes(hospedes);
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
-
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
-
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+            // Exibe a quantidade de hóspedes e o valor da diária
+            Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+            Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
