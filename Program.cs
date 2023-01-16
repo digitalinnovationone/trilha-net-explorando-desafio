826ef5a23667db9c8e700807cedb0884a78f6fef
Yas Souza <yasmin1014@hotmail.com>
2023-01-16 19:07:09 -0300

    Desafio-Hospedagem
    Completei o desafio e alterei a opção de informar os hóspedes e a suítes enquanto roda o programa e não no código como era antes.

--- Program.cs
+++ Program.cs
@@ -6,20 +6,36 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
-
-hospedes.Add(p1);
-hospedes.Add(p2);
+Console.Write("Informar quantidade de hóspedes: ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Hóspede #{i}:");
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine();
+    Console.Write("Sobrenome: ");
+    string sobrenome = Console.ReadLine();
+    hospedes.Add(new Pessoa(nome, sobrenome));
+}
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Console.Write("Informe o tipo da suíte: ");
+string tipoSuite = Console.ReadLine();
+Console.Write("Informe a capacidade da suíte: ");
+int capacidade = int.Parse(Console.ReadLine());
+Console.Write("Informe o valor da suíte: ");
+decimal valorDiaria = decimal.Parse(Console.ReadLine());
+Suite suite = new Suite(tipoSuite, capacidade, valorDiaria);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Console.Write("Informe quantos dias de reserva: ");
+int diasReservados = int.Parse(Console.ReadLine());
+Reserva reserva = new Reserva(diasReservados);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine("# Informação sobre sua hospedagem #");
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
