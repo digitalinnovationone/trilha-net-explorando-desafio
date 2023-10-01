e55af567d55c92348b91b53bb81d870021e75f5a
Homero Barbosa Neto <homerobneto@hotmail.co.uk>
2023-10-01 18:25:07 -0300

    Update Reserva.cs
    Clanning TODO comments

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,7 +15,6 @@ namespace DesafioProjetoHospedagem.Models
 
         public (bool, string) CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             bool sucesso = hospedes.Count <= Suite.Capacidade ? true : false;
             string[] mensagem = {$"Reserva realizada com sucesso!",$"Lamento, a suite seleciona pode acomodar apenas {Suite.Capacidade} pessoas.\nReserva para {hospedes.Count} pessoas não foi realizada!"};
             if (sucesso)
@@ -25,7 +24,6 @@ namespace DesafioProjetoHospedagem.Models
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 return (sucesso, mensagem[1]);
             }
         }
@@ -37,19 +35,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             return hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-
             decimal valor = Suite.ValorDiaria * DiasReservados; 
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-
             if (DiasReservados >= 10) 
             {
                 valor *= 0.9m; 
