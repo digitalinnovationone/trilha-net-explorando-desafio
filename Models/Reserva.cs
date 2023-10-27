b7b4ee27c557c5ba36be5122f3412775a82a2a5f
amrodrigues <42442977+amrodrigues@users.noreply.github.com>
2023-10-27 10:15:47 -0300

    Feitas as validações e contas para o funcionamento do projeto

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,10 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            int capacidade = Suite.Capacidade;
+            int qtdehospedes = hospedes.Count();
+
+            if (qtdehospedes <= capacidade)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +28,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new("Número de hóspedes maior que a capacidade da suíte.");
             }
         }
 
@@ -37,7 +41,10 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+
+            int qtdehospedes = Hospedes.Count();
+
+            return qtdehospedes;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +52,17 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
+           
             decimal valor = 0;
 
+            valor = Convert.ToDecimal(DiasReservados) * Suite.ValorDiaria;
+         
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = (valor * 10) / 100;
+                valor = valor-desconto;
             }
 
             return valor;
