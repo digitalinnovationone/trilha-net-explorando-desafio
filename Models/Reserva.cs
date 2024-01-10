d57e6e647ccff262884da5292a0b43a041a5c75f
Pedrolustosa <pedroeternalss@gmail.com>
2024-01-10 20:36:09 -0300

    refactor: Melhorias no código de exemplo
    - Utilização de inicializadores de listas para simplificar a criação e adição de hóspedes.
    - Refatoração do código de criação de hóspedes, tornando-o mais conciso.
    - Adição de formatação para exibição do valor da diária como uma moeda com duas casas decimais.
    - Aprimoramento nas mensagens de saída para maior legibilidade.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,17 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // IMPLEMENTAÇÃO
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // IMPLEMENTAÇÃO
+                throw new Exception("Capacidade insuficiente para o número de hóspedes.");
             }
         }
 
@@ -35,23 +36,23 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // IMPLEMENTAÇÃO
+            return Hospedes?.Count ?? 0;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
+            // Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // IMPLEMENTAÇÃO
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // IMPLEMENTAÇÃO
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= 0.9m; // Aplicando desconto de 10%
             }
 
             return valor;
--- Models/Suite.cs
+++ Models/Suite.cs
@@ -2,6 +2,10 @@ namespace DesafioProjetoHospedagem.Models
 {
     public class Suite
     {
+        private string _tipoSuite;
+        private int _capacidade;
+        private decimal _valorDiaria;
+
         public Suite() { }
 
         public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
@@ -10,9 +14,48 @@ namespace DesafioProjetoHospedagem.Models
             Capacidade = capacidade;
             ValorDiaria = valorDiaria;
         }
+       
 
-        public string TipoSuite { get; set; }
-        public int Capacidade { get; set; }
-        public decimal ValorDiaria { get; set; }
+        public string TipoSuite
+        {
+            get => _tipoSuite;
+            set
+            {
+                // Validar se o tipo da suite não é nulo ou vazio
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("O tipo da suíte não pode ser nulo ou vazio.");
+                }
+                _tipoSuite = value;
+            }
+        }
+
+        public int Capacidade
+        {
+            get => _capacidade;
+            set
+            {
+                // Validar se a capacidade é um número positivo
+                if (value <= 0)
+                {
+                    throw new ArgumentException("A capacidade da suíte deve ser um número positivo.");
+                }
+                _capacidade = value;
+            }
+        }
+
+        public decimal ValorDiaria
+        {
+            get => _valorDiaria;
+            set
+            {
+                // Validar se o valor da diária é um número positivo
+                if (value <= 0)
+                {
+                    throw new ArgumentException("O valor da diária deve ser um número positivo.");
+                }
+                _valorDiaria = value;
+            }
+        }
     }
 }
