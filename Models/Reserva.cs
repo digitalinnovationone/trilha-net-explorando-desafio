b98ccc6359dace482ffdfbf704b0708de7148526
marcelldac <dacmarcell@gmail.com>
2024-01-18 04:05:36 -0300

    feat: challenge

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,13 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (Hospedes.Capacity >= hospedes.Capacity)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new Exception("Capacidade é menor que o número de hóspedes recebido.");
             }
         }
 
@@ -35,23 +32,19 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
+            foreach(var hospede in Hospedes){
+                Console.WriteLine(hospede);
+            }
             return 0;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            int TenDays = 10;
+            if (DiasReservados >= TenDays)
             {
-                valor = 0;
+                valor *= 0.90M;
             }
 
             return valor;
